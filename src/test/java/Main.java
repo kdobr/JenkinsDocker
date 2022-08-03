87c18e749a5fe51d736074c84581f45196f68f21
kdobr <kdobr77@gmail.com>
2022-08-03 12:28:32 +0300

    add test 4

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,8 +1,11 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Main {
 @Test
     public void printHello(){
     System.out.println("Hello Docker");
+    //int a = 2*2;
+    Assertions.assertTrue(2*2==4);
 }
 }
